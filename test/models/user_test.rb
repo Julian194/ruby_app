2e97685cd8b48863b801dbf025cab1d17dc92e42
Julian194 <j.kaiser92@gmx.de>
2019-08-24 11:52:18 +0200

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -46,43 +46,71 @@ class UserTest < ActiveSupport::TestCase
      @user.email = invalid_address
      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
    end
- end
-
- test "email addresses should be unique" do
-   duplicate_user = @user.dup
-   duplicate_user.email = @user.email.upcase
-   @user.save
-   assert_not duplicate_user.valid?
- end
-
- test "email should be downcased before storing it" do
-   mixed_case_email = "Foo@ExAMPle.CoM"
-   @user.email = mixed_case_email
-   @user.save
-   assert_equal mixed_case_email.downcase,  @user.reload.email
- end
-
-test "password should be present (nonblank)" do
-  @user.password = @user.password_confirmation = " " * 6
-  assert_not @user.valid?
-end
-
-test "password should have a minimum length" do
-  @user.password = @user.password_confirmation = "a" * 5
-  assert_not @user.valid?
-end
-
-test "authenticated? should return false for a user with nil digest" do
-  assert_not @user.authenticated?(:remember, '')
-end
-
-test "associated microposts should be destroyed" do
-  @user.save
-  @user.microposts.create!(content: "Lorem ipsum")
-  assert_difference 'Micropost.count', -1 do
-    @user.destroy
   end
-end
 
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email should be downcased before storing it" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase,  @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?(:remember, '')
+  end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
+  
+  test "should follow and unfollow a user" do
+    julian = users(:julian)
+    dude = users(:dude)
+    assert_not julian.following?(dude)
+    julian.follow(dude)
+    assert julian.following?(dude)
+    assert dude.followers.include?(julian)
+    julian.unfollow(dude)
+    assert_not julian.following?(julian)
+  end
+
+  test "feed should have the right posts" do
+    julian = users(:julian)
+    dude = users(:dude)
+    lana = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert julian.feed.include?(post_following)
+    end
+    # Posts from self
+    julian.microposts.each do |post_self|
+      assert julian.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    dude.microposts.each do |post_unfollowed|
+      assert_not julian.feed.include?(post_unfollowed)
+    end
+  end
 
 end
